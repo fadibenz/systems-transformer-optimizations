cc642072f66c4815b1a04e4fd42ca52541255925
fadi benzaima <f.benzaima@esi-sba.dz>
2025-07-10 20:31:56 +0300

    Add option to run benchmarking with mixed-precision

--- systems/1_benchmarking_profiling/benchmarking_script.py
+++ systems/1_benchmarking_profiling/benchmarking_script.py
@@ -6,13 +6,15 @@ from transformer_implementation.model import BasicsTransformerLM
 from systems.utils import load_config, set_seed_everything
 import timeit
 import numpy as np
-
+import sys
+from contextlib import nullcontext
 
 def benchmark_operation(model:nn.Module,
                         data: torch.Tensor,
                         num_iterations:int,
                         warmup_iterations: int,
-                        full_run: bool) -> [float, float]:
+                        full_run: bool = False,
+                        mixed_precision: bool = False) -> [float, float]:
 
     """
     A function that benchmarks model run based on following params:
@@ -22,29 +24,34 @@ def benchmark_operation(model:nn.Module,
         warmup_iteration: number of iterations to warm up the model and allow for compilation
         full_run: whether to run the backward pass or not.
     """
+    if not torch.cuda.is_available():
+        print("Must run this code with GPU")
+        sys.exit(1)
 
     for _ in range(warmup_iterations):
         model(data)
 
-    if torch.cuda.is_available():
-        torch.cuda.synchronize()
+    torch.cuda.synchronize()
 
     time_list: list[float] = []
+
+    context_manager = torch.autocast(device_type="cuda", dtype=torch.bfloat16) if mixed_precision else nullcontext()
 
     for _ in range(num_iterations):
         start_time = timeit.default_timer()
 
-        output = model(data)
+        with context_manager:
+            output = model(data)
+            loss = output.mean()
 
         if full_run:
-            loss = output.mean()
             loss.backward()
 
-        if torch.cuda.is_available():
-            torch.cuda.synchronize()
+        torch.cuda.synchronize()
 
         run_time = timeit.default_timer() - start_time
         time_list.append(run_time)
+
     mean_time = np.mean(time_list)
     std_time = np.std(time_list)
 
@@ -69,6 +76,7 @@ def get_args():
     parser.add_argument('--iters', type=int, default=10)
     parser.add_argument('--warmup_iters', type=int, default=3)
     parser.add_argument('--full_run', action='store_true', help="Run both forward and backward pass")
+    parser.add_argument('--mixed_precision', action='store_true', help="Run with mixed-precision")
 
     # Oter
     parser.add_argument('--seed', type=int, default=2025)
@@ -115,7 +123,8 @@ if __name__ == "__main__":
         data=random_input_data,
         num_iterations=args.iters,
         warmup_iterations=args.warmup_iters,
-        full_run=args.full_run
+        full_run=args.full_run,
+        mixed_precision=args.mixed_precision
     )
 
     print("\n ----Finished benchmarking----")
