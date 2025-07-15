e303a8edce81ea6f6ea60d24f2944afcbb658c99
fadi benzaima <f.benzaima@esi-sba.dz>
2025-07-15 20:09:59 +0300

    fix: Add explicit dtype and device when creating ouput tensors

--- systems/flashattention2_triton/flashAttention2_triton_wrapper.py
+++ systems/flashattention2_triton/flashAttention2_triton_wrapper.py
@@ -20,14 +20,15 @@ class FlashAttention2Triton(torch.autograd.Function):
         assert Q.is_cuda and K.is_cuda and V.is_cuda, "Expected CUDA tensors"
         assert Q.is_contiguous() and K.is_contiguous() and V.is_contiguous(), "Tensors must be contiguous"
 
-        O = torch.empty_like(Q, requires_grad=True)
-        L = torch.empty((BATCH, N_QUERIES), device=Q.device, requires_grad=True)
+        O = torch.empty_like(Q)
+        L = torch.empty((BATCH, N_QUERIES), device=Q.device, dtype=torch.float32)
+
         Q_TILE_SIZE = 32
         K_TILE_SIZE = 32
 
 
         T_q = (N_QUERIES + Q_TILE_SIZE - 1) // Q_TILE_SIZE
-        scale = 1 / math.sqrt(D)
+        scale = 1.0 / math.sqrt(float(D))
 
         flash_fwd_kernel[(T_q, BATCH)](
             Q, K, V,
@@ -36,7 +37,7 @@ class FlashAttention2Triton(torch.autograd.Function):
             K.stride(0), K.stride(1), K.stride(2),
             V.stride(0), V.stride(1), V.stride(2),
             O.stride(0), O.stride(1), O.stride(2),
-            L.stride(0), L.stride(1), L.stride(2),
+            L.stride(0), L.stride(1),
             N_QUERIES, N_KEYS,
             scale,
             D,
