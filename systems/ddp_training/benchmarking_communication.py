8a98a32e0f770a01f81d5512b551d71f4f4a7e8f
fadi benzaima <f.benzaima@esi-sba.dz>
2025-07-20 16:17:21 +0300

    feat: Add modular code to benchmark communication overhaed of all-reduce operation

---
+++ systems/ddp_training/benchmarking_communication.py
@@ -0,0 +1,112 @@
+import os
+import timeit
+
+import torch
+import torch.multiprocessing as mp
+import torch.distributed as dist
+import numpy as np
+from systems.ddp_training.utils import find_free_port, setup, construct_config
+
+MB = 1024 * 1024
+GB = 1024 * MB
+
+
+def distributed_benchmark(rank: int,
+                          world_size: int,
+                          backend: str,
+                          device_type: str,
+                          tensor_size_bytes: int,
+                          port: int,
+                          num_iterations: int = 20,
+                          warmup_iterations: int = 5):
+    setup(rank, world_size, backend, port)
+
+    if device_type == "gpu":
+        device = torch.device(f"cuda:{rank}")
+        torch.cuda.set_device(device)
+    else:
+        device = torch.device("cpu")
+
+    num_elements = tensor_size_bytes // 4
+    data = torch.randint(0, 100, (num_elements,), dtype=torch.float32).to(device)
+
+    for _ in range(warmup_iterations):
+        dist.all_reduce(data, async_op=False)
+
+    if device_type == "gpu":
+        torch.cuda.synchronize()
+
+    dist.barrier()
+
+    start_time = timeit.default_timer()
+
+    for _ in range(num_iterations):
+        dist.all_reduce(data, async_op=False)
+
+    if device_type == "gpu":
+        torch.cuda.synchronize()
+
+    end_time = timeit.default_timer() - start_time
+
+    time_list = [None] * world_size
+    dist.all_gather_object(time_list, end_time)
+
+    if rank == 0:
+        avg_total_time = np.mean(time_list)
+        avg_time_s = avg_total_time / num_iterations
+
+        bus_bandwidth_gbps = (tensor_size_bytes * 2) / avg_time_s / GB
+
+        print(
+            f"  -> Avg Time: {avg_time_s * 1000:.4f} ms | "
+            f"Effective Bandwidth: {bus_bandwidth_gbps:.2f} GB/s"
+        )
+
+    dist.destroy_process_group()
+
+
+if __name__ == "__main__":
+    CONFIGS_TO_RUN = construct_config("systems/configs/distributed_benchmarking")
+
+    num_gpus = torch.cuda.device_count()
+    num_cores = os.cpu_count()
+
+    print("\nStarted benchmarking")
+
+    for config in CONFIGS_TO_RUN:
+        world_size = config['world_size']
+
+        if config['backend'] == 'nccl' and num_gpus < world_size:
+            print(
+                f"Skipping config ({world_size} procs): "
+                f"Requires {world_size} GPUs, but only {num_gpus} are available."
+            )
+            continue
+
+        if config['backend'] == 'gloo' and num_cores < world_size:
+            print(
+                f"Skipping config ({config['backend']}, {world_size} procs): "
+                f"Requires {world_size} CPU cores, but only {num_cores} are available."
+            )
+            continue
+
+        print(f"Config: Backend={config['backend']}, "
+              f"Processes={world_size}, Size={config['tensor_size_bytes'] / MB:.0f}MB")
+
+        free_port = find_free_port()
+
+        mp.spawn(
+            fn=distributed_benchmark,
+            args=(
+                world_size,
+                config['backend'],
+                config['device'],
+                config['tensor_size_bytes'],
+                free_port
+            ),
+            nprocs=world_size,
+            join=True
+        )
+        print("-" * 60)
+
+    print("\nFinished benchmarking")
---
+++ systems/ddp_training/utils.py
@@ -0,0 +1,35 @@
+import os
+import torch.distributed as dist
+import socket
+from systems.utils import load_config
+from itertools import product
+
+def setup(rank, world_size, backend, port):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = str(port)
+
+    dist.init_process_group(
+        backend=backend,
+        rank=rank,
+        world_size=world_size
+    )
+
+def find_free_port():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('localhost', 0))
+        return s.getsockname()[1]
+
+def construct_config(file_path):
+    config_data = load_config(file_path)
+    CONFIGS_TO_RUN = []
+    for benchmark in config_data['benchmarks']:
+        backend = benchmark['backend']
+        device = benchmark['device']
+        for world_size, tensor_size_mb in product(benchmark['world_sizes'], benchmark['tensor_sizes_mb']):
+            CONFIGS_TO_RUN.append({
+                'backend': backend,
+                'device': device,
+                'world_size': world_size,
+                'tensor_size_bytes': tensor_size_mb * MB
+            })
+
