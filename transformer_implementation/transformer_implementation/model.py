9f3ace8b93a4d666ea77f29eeec45ab28e8795af
fadi benzaima <f.benzaima@esi-sba.dz>
2025-07-21 15:29:17 +0300

    refactor: use pytorch scaled_dot_product until I implement head support in my triton kernel

--- transformer_implementation/transformer_implementation/model.py
+++ transformer_implementation/transformer_implementation/model.py
@@ -12,8 +12,7 @@ import torch
 import torch.nn as nn
 from torch import Tensor
 from jaxtyping import Float, Bool, Int
-from systems.flashattention2_triton.flashAttention2_triton_wrapper import FlashAttention2Triton
-
+from torch.nn.functional import scaled_dot_product_attention
 from .nn_utils import softmax
 
 logger = logging.getLogger(__name__)
@@ -397,32 +396,39 @@ class SwiGLU(nn.Module):
         return self.w2(silu(self.w1(x)) * self.w3(x))
 
 
-def scaled_dot_product_attention(
-    Q: Float[Tensor, " ... queries d_k"],
-    K: Float[Tensor, " ... keys    d_k"],
-    V: Float[Tensor, " ... keys    d_v"],
-    mask: Bool[Tensor, " ... queries keys"] | None = None,
-) -> Float[Tensor, " ... queries d_v"]:
-    """Scaled dot-product attention.
-
-    This function implements Eq. 1 of the Transformer paper.
-
-    Args:
-        Q: Tensor of queries, may have any number of leading dimensions.
-        K: Tensor of keys, sharing leading dimensions with Q.
-        V: Tensor of values, sharding leading dimensions with Q and K.
-        mask: An (optional) mask of shape (..., seq_len, seq_len).
-            Attention scores for positions with a mask value of `False` should
-            be masked out, i.e., not affect the softmaxed attention probabilities.
-
-    Returns:
-        torch.FloatTensor of shape (..., seq_len, value_dimension)
-        with the output of running your scaled dot product attention
-        implementation with the provided key, query, and value tensors.
-    """
-
-
-    return FlashAttention2Triton.apply(Q, K, V, True)
+# def scaled_dot_product_attention(
+#     Q: Float[Tensor, " ... queries d_k"],
+#     K: Float[Tensor, " ... keys    d_k"],
+#     V: Float[Tensor, " ... keys    d_v"],
+#     mask: Bool[Tensor, " ... queries keys"] | None = None,
+# ) -> Float[Tensor, " ... queries d_v"]:
+#     """Scaled dot-product attention.
+#
+#     This function implements Eq. 1 of the Transformer paper.
+#
+#     Args:
+#         Q: Tensor of queries, may have any number of leading dimensions.
+#         K: Tensor of keys, sharing leading dimensions with Q.
+#         V: Tensor of values, sharding leading dimensions with Q and K.
+#         mask: An (optional) mask of shape (..., seq_len, seq_len).
+#             Attention scores for positions with a mask value of `False` should
+#             be masked out, i.e., not affect the softmaxed attention probabilities.
+#
+#     Returns:
+#         torch.FloatTensor of shape (..., seq_len, value_dimension)
+#         with the output of running your scaled dot product attention
+#         implementation with the provided key, query, and value tensors.
+#     """
+#
+#     d_k = K.shape[-1]
+#     attention_scores = einsum(Q, K, "... query d_k, ... key d_k -> ... query key") / math.sqrt(d_k)
+#
+#     if mask is not None:
+#         attention_scores = torch.where(mask, attention_scores, float("-inf"))
+#
+#     attention_weights = softmax(attention_scores, dim=-1)  # Softmax over the key dimension
+#
+#     return einsum(attention_weights, V, "... query key, ... key d_v ->  ... query d_v")
 
 
 class CausalMultiHeadSelfAttention(nn.Module):
@@ -506,7 +512,7 @@ class CausalMultiHeadSelfAttention(nn.Module):
         causal_mask = qi >= kj  # (query, key)
 
         # Shape: (..., num_heads, sequence_length, d_k)
-        attn_output = scaled_dot_product_attention(K=K, Q=Q, V=V, mask=causal_mask)
+        attn_output = scaled_dot_product_attention(K, Q, V, attn_mask=causal_mask)
 
         # Concatenate the attention output from all heads.
         # (..., sequence_length, num_heads * d_v).
