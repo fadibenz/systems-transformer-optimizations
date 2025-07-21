0ee650a5d34a4ff977f40613ad807e661358f54b
fadi benzaima <f.benzaima@esi-sba.dz>
2025-07-21 14:49:56 +0300

    feat: Add benchmarking code with ddp and flattenting of tensors to reduce overhead

---
+++ systems/ddp_training/minimal_ddp_flat_benchmarking.py
@@ -0,0 +1,126 @@
+import torch
+import torch.multiprocessing as mp
+import torch.distributed as dist
+import numpy as np
+
+from systems.benchmarking_profiling.utils import get_args
+from systems.ddp_training.utils import setup, forward_backward_step, print_logs, prepare_local_data
+from systems.utils import load_config, set_seed_everything
+import sys
+import timeit
+from transformer_implementation.data import get_batch
+from transformer_implementation.model import BasicsTransformerLM
+from transformer_implementation.optimizer import AdamW
+
+from torch._utils import _flatten_dense_tensors, _unflatten_dense_tensors
+
+def train_model(rank: int,
+                world_size: int,
+                config: dict,
+                args,
+                x: torch.Tensor,
+                y: torch.Tensor,
+                ):
+    setup(rank, world_size, "nccl")
+
+    device = torch.device(f"cuda:{rank}")
+    torch.cuda.set_device(device)
+    # Data
+    data, targets = prepare_local_data(x, y, rank, device, world_size)
+    # Model
+    model_config = args.model
+    model = BasicsTransformerLM(
+        d_model=model_config["d_model"],
+        num_layers=model_config["num_layers"],
+        num_heads=model_config["num_heads"],
+        d_ff=model_config["d_ff"],
+
+        context_length=args.context_length,
+        vocab_size=config["vocab_size"],
+        rope_theta=config["rope_theta"]
+    )
+    model.to(device)
+
+    # Broadcast initial params
+    param_data = [param.data for param in model.parameters()]
+    flattened_param_data = _flatten_dense_tensors(param_data)
+    dist.broadcast(flattened_param_data, src=0)
+    unflattened_param_data = _unflatten_dense_tensors(flattened_param_data, param_data)
+
+    for original_data, new_data in zip(param_data, unflattened_param_data):
+        original_data.copy_(new_data)
+
+    optimizer = AdamW(model.parameters())
+
+    for _ in range(args.warmup_iters):
+        forward_backward_step(model, data, targets, optimizer)
+
+        grads = [param.grad for param in model.parameters() if param.grad is not None]
+        flattened_tensor = _flatten_dense_tensors(grads)
+        dist.all_reduce(flattened_tensor, op=dist.ReduceOp.AVG)
+        unflattened_tensors = _unflatten_dense_tensors(flattened_tensor, grads)
+
+        for original_grad, average_grad in zip(grads, unflattened_tensors):
+            original_grad.copy_(average_grad)
+
+        optimizer.step()
+
+    torch.cuda.synchronize()
+    end_time_reduce = 0
+    start_time_e2e = timeit.default_timer()
+
+    for _ in range(args.iters):
+        forward_backward_step(model, data, targets, optimizer)
+
+        start_time_reduce = timeit.default_timer()
+        grads = [param.grad for param in model.parameters() if param.grad is not None]
+        flattened_tensor = _flatten_dense_tensors(grads)
+        dist.all_reduce(flattened_tensor, op=dist.ReduceOp.AVG)
+        unflattened_tensors = _unflatten_dense_tensors(flattened_tensor, grads)
+
+        for original_grad, average_grad in zip(grads, unflattened_tensors):
+            original_grad.copy_(average_grad)
+
+        torch.cuda.synchronize()
+        end_time_reduce += timeit.default_timer() - start_time_reduce
+
+        optimizer.step()
+
+    torch.cuda.synchronize()
+    end_time_e2e = timeit.default_timer() - start_time_e2e
+
+    print_logs(end_time_e2e, end_time_reduce, device, rank, args.iters, world_size)
+
+    dist.destroy_process_group()
+
+
+if __name__ == "__main__":
+
+    args = get_args()
+    if not torch.cuda.is_available():
+        print("Must run this code with GPU")
+        sys.exit(1)
+
+    set_seed_everything(args.seed)
+    world_size = torch.cuda.device_count()
+
+    config = load_config("systems/configs/model_sizing.YAML")
+
+    random_id_tokens = np.random.randint(0, config["vocab_size"], config["batch_size"] * args.context_length)
+
+    x, y = get_batch(random_id_tokens, config["batch_size"], args.context_length, device="cpu")
+
+    print("\n Started benchmarking naive DDP")
+    print(60 * "-")
+    mp.spawn(
+        fn=train_model,
+        args=(world_size,
+              config,
+              args,
+              x, y,
+              ),
+        nprocs=world_size,
+        join=True
+    )
+    print(60 * "-")
+    print("\n Finished benchmarking naive DDP")
